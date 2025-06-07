7c4cdeb0d770686271b176c0ebf80c584161d875
jp <161966976+JoaumPdr@users.noreply.github.com>
2025-06-07 01:04:42 -0300

    Update vite.config.ts

--- Projeto S05/project/vite.config.ts
+++ Projeto S05/project/vite.config.ts
@@ -1,8 +1,11 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Adicione o nome do seu repositório aqui
+  base: '/ProjetoS05/', // Exemplo: se a URL for https://joaumpdr.github.io/ProjetoS05/
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
